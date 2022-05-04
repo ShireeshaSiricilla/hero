4a73b53aec0e30e89748e229021d8af8a5c9034c
ShireeshaSiricilla <103118040+ShireeshaSiricilla@users.noreply.github.com>
2022-05-04 21:03:54 +0530

    Add files via upload

---
+++ mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from "./app.component";
+export const HEROES: Hero[]=[
+    {id:21,name:'shirisha'},
+    {id:22,name:'arya'},
+    {id:23,name:'divya'},
+    {id:24,name:'soumya'},
+    
+]
